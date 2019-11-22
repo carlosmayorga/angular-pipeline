6e2df4f0cf4f75df6e711cc0ca4b2e2265cb8f8f
Carlos Javier <carlosjavier@Carlos-Javier-MacBook-Pro.local>
2019-11-21 22:48:59 -0500

    modify .circleci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-pipeline'`, () => {
+  it(`should have as title 'Pipeline walkthrough'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-pipeline');
+    expect(app.title).toEqual('Pipeline walkthrough');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render a img', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-pipeline!');
+    expect(compiled.querySelector('img'));
   });
 });
