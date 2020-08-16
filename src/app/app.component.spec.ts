5639616152377e3d09d2277c01db3874a42fc796
carlosmayorga <localhost8027@gmail.com>
2020-08-15 21:48:20 -0500

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Pipeline walkthrough'`, () => {
+  it(`should have as title 'Maestría en Ingeniería de Software'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Pipeline walkthrough');
+    expect(app.title).toEqual('Maestría en Ingeniería de Software');
   });
 
   it('should render a img', () => {
