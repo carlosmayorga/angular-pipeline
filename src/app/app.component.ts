6466d1d00fb7294fe6c0901abcbf801fd8450998
carlosmayorga <localhost8027@gmail.com>
2020-08-17 10:32:49 -0500

    update text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Maestría en Ingeniería de Software';
+  title = 'Maestria en Ingeniería de Software';
 }
